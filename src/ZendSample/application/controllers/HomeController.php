884fee731d9d3a91d7fb3b8ffd9f70e033f7f8a9
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2021-12-12 16:56:23 +0900

    user preDispatch() method

--- src/ZendSample/application/controllers/HomeController.php
+++ src/ZendSample/application/controllers/HomeController.php
@@ -6,22 +6,29 @@ require_once APPLICATION_PATH . '/utilities/SessionNamespace.php';
 
 class HomeController extends Zend_Controller_Action {
 
+    public ?string $userId = null;
+
     public function init(): void {
         $layoutConfig = new Zend_Config_Ini(APPLICATION_PATH . '/configs/common-layout-config.ini', 'layout');
         Zend_Layout::startMvc($layoutConfig);
     }
-    
-    public function indexAction(): void {
-        // session check
-        if (! Zend_Session::sessionExists()) {
+
+    public function preDispatch(): void {
+        parent::preDispatch();
+
+        // Login check
+        $defaultNamespace = SessionNamespace::getInstance()->getNamespace(DEFAULT_NAMESPACE);
+        try {
+            $user = User::cast(unserialize($defaultNamespace->user));
+        } catch(Exception $e) {
             $this->_redirect('/welcome/signin');
             return;
         }
-
-        $defaultNamespace = SessionNamespace::getInstance()->getNamespace(DEFAULT_NAMESPACE);
-        $user = User::cast(unserialize($defaultNamespace->user));
-        $userId = $user->getUserId();
-        $allTasks = TaskLogic::getAllTasks($userId);
+        $this->userId = $user->getUserId();
+    }
+    
+    public function indexAction(): void {
+        $allTasks = TaskLogic::getAllTasks($this->userId);
         $this->view->assign('isLogin', '1');
         $this->view->assign('taskStatus', array(TASK_TO_DO, TASK_IN_PROGRESS, TASK_DONE));
         $this->view->assign('allTasks', $allTasks);
@@ -29,16 +36,6 @@ class HomeController extends Zend_Controller_Action {
     }
 
     public function updatetaskAction(): void {
-        // session check
-        if (! Zend_Session::sessionExists()) {
-            $this->_redirect('/welcome/signin');
-            return;
-        }
-
-        $defaultNamespace = SessionNamespace::getInstance()->getNamespace(DEFAULT_NAMESPACE);
-        $user = User::cast(unserialize($defaultNamespace->user));
-        $userId = $user->getUserId();
-
         if ($this->getRequest()->isGet()) {// To home page
             $this->_redirect('/home/index');
             return;
@@ -49,7 +46,7 @@ class HomeController extends Zend_Controller_Action {
             $taskStatus = $this->_getParam(TASK_STATUS);
 
             // Update a task on DB
-            if (TaskLogic::updateTask($userId, $taskId, $taskTitle, $taskContent, $taskStatus)) {// Success
+            if (TaskLogic::updateTask($this->userId, $taskId, $taskTitle, $taskContent, $taskStatus)) {// Success
                 $this->_redirect('/home/index');
                 return;
             } else {// Failure
@@ -61,16 +58,7 @@ class HomeController extends Zend_Controller_Action {
     }
 
     public function newtaskAction(): void {
-        // session check
-        if (! Zend_Session::sessionExists()) {
-            $this->_redirect('/welcome/signin');
-            return;
-        }
 
-        $defaultNamespace = SessionNamespace::getInstance()->getNamespace(DEFAULT_NAMESPACE);
-        $user = User::cast(unserialize($defaultNamespace->user));
-        $userId = $user->getUserId();
-        
         if ($this->getRequest()->isGet()) {// To new task page
             return;
         } else {// Register new task and to home page
@@ -79,7 +67,7 @@ class HomeController extends Zend_Controller_Action {
             $taskStatus = $this->_getParam(TASK_STATUS);
 
             // Register new task to DB
-            if (TaskLogic::registerTask($userId, $taskTitle, $taskContent, $taskStatus)) {// Success in registering a new task
+            if (TaskLogic::registerTask($this->userId, $taskTitle, $taskContent, $taskStatus)) {// Success in registering a new task
                 $this->_redirect('/home/index');
                 return;
             } else {// Failure
@@ -90,15 +78,6 @@ class HomeController extends Zend_Controller_Action {
     }
 
     public function deletetaskAction(): void {
-        // session check
-        if (! Zend_Session::sessionExists()) {
-            $this->_redirect('/welcome/signin');
-            return;
-        }
-
-        $defaultNamespace = SessionNamespace::getInstance()->getNamespace(DEFAULT_NAMESPACE);
-        $user = User::cast(unserialize($defaultNamespace->user));
-        $userId = $user->getUserId();
 
         if ($this->getRequest()->isGet()) {// Redirect to indexAction
             $this->_redirect('/home/index');
@@ -106,7 +85,7 @@ class HomeController extends Zend_Controller_Action {
             $taskId = $this->_getParam(TASK_ID);
 
             // Delete the task on DB
-            if (TaskLogic::deleteTask($userId, $taskId)) {// Success in deleting the task
+            if (TaskLogic::deleteTask($this->userId, $taskId)) {// Success in deleting the task
                 $this->_redirect('/home/index');
                 return;
             } else {// Failure
